16143f9adcbe55b8210d9ea01748f76c0a91c426
davidbarone <davidbarone@live.com>
2021-07-11 17:23:42 +1000

    Opcodes tidy up (setnz)

--- src/src/cpu/OpCodeGenRule.ts
+++ src/src/cpu/OpCodeGenRule.ts
@@ -1,23 +1,16 @@
 import OpCodeGenParams from "./OpCodeGenParams";
 
 export default class OpCodeGenRule {
-    public constructor({ instruction, addressMode, operation, affectZFlag = false, affectNFlag = false, affectVFlag = false, write = false, CFlagFunc = null }: OpCodeGenParams) {
-      this.Instruction = instruction;
-      this.AddressMode = addressMode;
-      this.Operation = operation;
-      this.AffectNFlag = affectNFlag;
-      this.AffectVFlag = affectVFlag;
-      this.AffectZFlag = affectZFlag;
-      this.CFlagFunc = CFlagFunc;
-      this.Write = write;
-    }
-    public Instruction: string = "" // The 3-letter instruction
-    public AddressMode: string = "" // The address mode
-    public Operation: string = "" // The operation / logic to execute
-    public AffectZFlag?: boolean = false // Does the operation affect the zero flag?
-    public AffectNFlag: boolean = false // Does the operation affect the negative flag?
-    public AffectVFlag: boolean = false // Does the operation affect the overflow flag?
-    public CFlagFunc: Function | null // Does the operation affect the negative flag?
-    public Write: boolean = false;    // does instruction perform a write operation?
+  public constructor({ instruction, addressMode, operation, cycles, write = false }: OpCodeGenParams) {
+    this.Instruction = instruction;
+    this.AddressMode = addressMode;
+    this.Operation = operation;
+    this.Write = write;
+    this.Cycles = cycles;
   }
-  
+  public Instruction: string = "" // The 3-letter instruction
+  public AddressMode: string = "" // The address mode
+  public Operation: string = "" // The operation / logic to execute
+  public Write: boolean = false;    // does instruction perform a write operation?
+  public Cycles: number = 0;
+}
