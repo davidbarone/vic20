943ae673607b9eccf0229debe4bde0bf6ca0ef6d
davidbarone <davidbarone@live.com>
2021-07-11 18:16:33 +1000

    adding cycles on opcodes.

--- src/src/cpu/OpCodeGenParams.ts
+++ src/src/cpu/OpCodeGenParams.ts
@@ -1,11 +1,7 @@
 export default interface OpCodeGenParams {
-    instruction: string;
-    addressMode: string;
-    operation: string;
-    affectZFlag?: boolean;
-    affectNFlag?: boolean;
-    affectVFlag?: boolean;
-    CFlagFunc?: Function | null;
-    write?: boolean;
-  }
-  
+  instruction: string;
+  addressMode: string;
+  operation: string;
+  write?: boolean;
+  cycles: number;
+}
